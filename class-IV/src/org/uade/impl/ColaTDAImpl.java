2e6c9cdc0f130d7e353e3b353bb9ae039b054793
asacco <andres.sacco@travelx.io>
2024-08-31 09:52:13 -0300

    code of class IV

--- class-IV/src/org/uade/impl/ColaTDAImpl.java
+++ class-IV/src/org/uade/impl/ColaTDAImpl.java
@@ -4,40 +4,33 @@ import org.uade.api.ColaTDA;
 
 public class ColaTDAImpl implements ColaTDA {
 
-    private int[] arr;
-    private int frente;
-    private int fin;
 
     // Inicializamos la cola con un tamaño fijo de 100 elementos
     @Override
     public void inicializarCola() {
-        arr = new int[100];
-        frente = 0;
-        fin = 0;
     }
 
     // Añadimos un elemento al final de la cola
     @Override
     public void acolar(int x) {
-        arr[fin] = x;
-        fin++;
+
     }
 
     // Removemos el primer elemento de la cola
     @Override
     public void desacolar() {
-        frente++;
+
     }
 
     // Devolvemos el primer elemento de la cola
     @Override
     public int primero() {
-        return arr[frente];
+    	return 0;
     }
 
     // Verificamos si la cola está vacía
     @Override
     public boolean colaVacia() {
-        return frente == fin;
+        return true;
     }
 }
--- class-IV/src/org/uade/impl/PilaTDAImpl.java
+++ class-IV/src/org/uade/impl/PilaTDAImpl.java
@@ -4,36 +4,27 @@ import org.uade.api.PilaTDA;
 
 public class PilaTDAImpl implements PilaTDA {
 
-    private int[] arr;
-    private int indice;
-
     @Override
     public void inicializarPila() {
-        arr = new int[100];
-        indice = 0;
     }
 
     @Override
     public void apilar(int x) {
-        arr[indice] = x;
-        indice++;
     }
 
     @Override
     public void desapilar() {
-        if (!pilaVacia()) {
-            indice--;
-        }
+  
     }
 
     @Override
     public int tope() {
-        return arr[indice - 1];
+        return 0;
     }
 
     @Override
     public boolean pilaVacia() {
-        return indice == 0;
+        return true;
     }
 }
 
